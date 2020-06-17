25bbc995688e4966418aa20495144a2ea1d7c409
Henry Westfall <henrywestfall@Bailes-MacBook-Pro.local>
2020-06-17 01:35:11 -0600

    work on rocket

--- rocket.py
+++ rocket.py
@@ -2,6 +2,7 @@ import math
 
 import pymunk
 from pymunk.pyglet_util import DrawOptions
+from pymunk.vec2d import Vec2d
 
 import pyglet
 
@@ -11,99 +12,95 @@ options = DrawOptions()
 space = pymunk.Space()
 space.gravity = 0, 0
 
+def centroid(vertices, weights=None):
+    num = len(vertices)
+    if weights is None:
+        weights = [1 / num] * num
+    
+    xsum = 0
+    ysum = 0
+    for i, v in enumerate(vertices):
+        xsum += v[0] * weights[i]
+        ysum += v[1] * weights[i]
+    
+    return (xsum, ysum)
+
 
 class Part:
     STACKED = 0
     RADIAL = 1
 
-    def __init__(self, space, body, shape):
-        self.body = body
-        self.shape = shape
-        self.space = space
-        self.space.add(self.body, self.shape)
-        self.joints = []
+    def __init__(self):
+        self.vertices = []
 
         self.mount = Part.STACKED
 
+        self.mass = 0
         self.radial_size = 0 # only applies to circular parts
         self.height = 0
 
-        # neighbor Parts
-        self.part_below = (None, [])
-        self.part_above = (None, [])
-        self.mounted_parts = set()
 
-    def connect_to(self, other):
-        """
-        Connect the parts (only works for certain stacked parts currently)
-        """
+class Rocket:
+    def __init__(self):
+        self.parts = []
 
-        if self.mount == Part.STACKED:
-            left_joint = pymunk.PinJoint(self.body, other.body, anchor_a=(self.radial_size, 0))
-            right_joint = pymunk.PinJoint(self.body, other.body, anchor_b=(other.radial_size, 0))
-            joints = [left_joint, right_joint]
-            self.space.add(*joints)
+    def add_part(self, part):
+        self.parts.append(part)
 
-            self.joints += joints
+    def get_body_and_shape(self):
+        # compile part data
+        vertices = []
+        mass = 0
+        centroids_map = {}
+        for p in self.parts:
+            vertices += p.vertices
+            mass += p.mass
+            centroids_map[centroid(p.vertices)] = p.mass
 
-            if (other.part_above[0] != self) or (other.part_below[0] != self):
-                other.joints += [left_joint, right_joint]
-                if self.body.position.y > other.body.position.y: # we are above them
-                    for j in joints:
-                        j.distance = math.hypot(self.height, self.radial_size)
-                    other.part_above = (self, joints)
-                    self.part_below = (other, joints)
-                else:
-                    for j in joints:
-                        j.distance = math.hypot(other.height, other.radial_size)
-                    other.part_below = (self, joints)
-                    self.part_above = (other, joints)
+        # calculate center of mass
+        weighted_centroids_map = {c: m / mass for c, m in centroids_map.items()}
+        centroids = list(weighted_centroids_map.keys())
+        weights = [weighted_centroids_map[c] for c in centroids]
+        com = centroid(centroids, weights)
 
-    def disconnect_from(self, other):
-        joints = None
-        if other is self.part_above:
-            joints = self.part_above[1]
-            self.part_above = (None, [])
-            other.part_below = (None, [])
-        elif other is self.part_below:
-            joints = self.part_below[1]
-            self.part_below = (None, [])
-            other.part_above = (None, [])
+        # calculate offset
+        vertices = list(set(vertices))
+        center = centroid(vertices)
+        offset = Vec2d(center) - Vec2d(com)
+
+        # create body
+        body = pymunk.Body(mass)
+        shape = pymunk.Poly(body, vertices)
+        body.moment = pymunk.moment_for_poly(mass, vertices, offset)
+        body.position = 450, 300
+
+        return body, shape, body.local_to_world(com)
+
+pod = Part()
+pod.vertices = [(0, 0), (45, 90), (90, 0)]
+pod.mass = 5
+
+engine = Part()
+engine.vertices = [(0, 0), (90, 0), (0, -100), (90, -100)]
+engine.mass = 15
+
+rocket = Rocket()
+rocket.parts = [pod, engine]
+
+body, shape, com = rocket.get_body_and_shape()
+space.add(body, shape)
+
+label = pyglet.text.Label("Center Of Mass",
+                          font_size=8,
+                          x=com[0], y=com[1],
+                          anchor_x="center", anchor_y="center")
         
-        if not (joints is None):
-            for j in joints:
-                other.joints.remove(j)
-                self.joints.remove(j)
-                self.space.remove(j)
-
-
-# pod part
-mass = 10000
-pod = pymunk.Body(mass)
-vertices = [(0, 0), (45, 90), (90, 0)]
-shape = pymunk.Poly(pod, vertices)
-pod.moment = pymunk.moment_for_poly(mass, vertices)
-pod.position = 450, 300
-pod_part = Part(space, pod, shape)
-pod_part.radial_size = 90
-
-# random box part
-mass = 10
-box = pymunk.Body(mass)
-vertices = [(0, 0), (90, 0), (90, 30), (0, 30)]
-shape = pymunk.Poly(box, vertices)
-box.moment = pymunk.moment_for_poly(mass, shape.get_vertices())
-box.position = 450, 270
-box_part = Part(space, box, shape)
-box_part.radial_size = 90
-box_part.height = 30
-
-pod_part.connect_to(box_part)
 
 @window.event
 def on_draw():
     window.clear()
     space.debug_draw(options)
+    label.draw()
 
 def update(dt):
     space.step(dt)
