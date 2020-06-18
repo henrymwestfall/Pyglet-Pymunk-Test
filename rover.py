7b734588c98e91bdfbc7269fce8ee808844485ba
Henry Westfall <henrymwestfall@gmail.com>
2020-06-17 21:05:02 -0600

    work on rover

--- rover.py
+++ rover.py
@@ -9,9 +9,106 @@ from pymunk.pygame_util import DrawOptions, to_pygame
 import pygame
 from pygame.locals import *
 
-from cv2 import VideoWriter, VideoWriter_fourcc
-
 import numpy as np
+
+def create_pogo(space, x, y):
+    pos = Vec2d(x, y)
+
+    # carriage
+    mass = 100
+    size = (50, 30)
+    moment = pymunk.moment_for_box(mass, size)
+    carriage_b = pymunk.Body(mass, moment)
+    carriage_s = pymunk.Poly.create_box(carriage_b, size)
+    carriage_s.color = 255, 0, 0
+    space.add(carriage_b, carriage_s)
+    sprites.append((carriage_b, carriage_s))
+
+    carriage_b.position = pos
+
+def create_pogo1(space, x, y):
+    pos = Vec2d(x, y)
+
+    # carriage
+    mass = 100
+    size = (50, 30)
+    moment = pymunk.moment_for_box(mass, size)
+    carriage_b = pymunk.Body(mass, moment)
+    carriage_s = pymunk.Poly.create_box(carriage_b, size)
+    carriage_s.color = 255, 0, 0
+    space.add(carriage_b, carriage_s)
+    sprites.append((carriage_b, carriage_s))
+
+    # left leg
+    mass = 20
+    left_leg_b = pymunk.Body(mass)
+    size = (10, 100)
+    left_leg_s = pymunk.Poly.create_box(left_leg_b, size)
+    left_leg_s.color = 255, 0, 0
+    space.add(left_leg_b, left_leg_s)
+    sprites.append((left_leg_b, left_leg_s))
+
+
+    # right leg
+    mass = 20
+    right_leg_b = pymunk.Body(mass)
+    size = (10, 100)
+    right_leg_s = pymunk.Poly.create_box(right_leg_b, size)
+    right_leg_s.color = 255, 0, 0
+    space.add(right_leg_b, right_leg_s)
+    sprites.append((right_leg_b, right_leg_s))
+
+    # left wheel
+    mass = 10
+    radius = 10
+    moment = pymunk.moment_for_circle(mass, 0, radius)
+    wheel1_b = pymunk.Body(mass, moment)
+    wheel1_s = pymunk.Circle(wheel1_b, radius)
+    wheel1_s.friction = 1.0
+    wheel1_s.color = 0, 0, 0
+    space.add(wheel1_b, wheel1_s)
+    sprites.append((wheel1_b, wheel1_s))
+
+    # right wheel
+    mass = 10
+    radius = 10
+    moment = pymunk.moment_for_circle(mass, 0, radius)
+    wheel2_b = pymunk.Body(mass, moment)
+    wheel2_s = pymunk.Circle(wheel2_b, radius)
+    wheel2_s.friction = 1.0
+    wheel2_s.color = 0, 0, 0
+    space.add(wheel2_b, wheel2_s)
+    sprites.append((wheel2_b, wheel2_s))
+
+    # stick
+    mass = 20
+    size = (40, 150)
+    stick_b = pymunk.Body(mass)
+    stick_s = pymunk.Poly.create_box(stick_b, size)
+    stick_s.color = 0, 0, 255
+    space.add(stick_b, stick_s)
+    sprites.append((stick_b, stick_s))
+
+    carriage_b.position = pos + (0, 100)
+    left_leg_b.position = pos + (-25, 50)
+    right_leg_b.position = pos + (25, 50)
+    wheel1_b.position = pos + (-25, -50)
+    wheel2_b.position = pos + (25, -50)
+    stick_b.position = pos + (0, -50)
+
+    space.add([
+        pymunk.PinJoint(carriage_b, left_leg_b, (0, 0), (0, 100)),
+        pymunk.PinJoint(carriage_b, left_leg_b, (25, 0), (0, 100)),
+
+        pymunk.PinJoint(carriage_b, right_leg_b, (0, 0), (0, 100)),
+        pymunk.PinJoint(carriage_b, right_leg_b, (25, 0), (0, 100)),
+
+        pymunk.PinJoint(left_leg_b, wheel1_b, (5, 0), (0, 0)),
+        pymunk.PinJoint(right_leg_b, wheel2_b, (5, 0), (0, 0))
+    ])
+
+    return carriage_b
+
 
 def create_rover(space, x, y):
     pos = Vec2d(x, y)
@@ -41,15 +138,15 @@ def create_rover(space, x, y):
     sprites.append((wheel2_b, wheel2_s))
 
     # third wheel
-    mass = 10
-    radius = 25
-    moment = pymunk.moment_for_circle(mass, 20, radius)
-    wheel3_b = pymunk.Body(mass, moment)
-    wheel3_s = pymunk.Circle(wheel3_b, radius)
-    wheel3_s.friction = 1.5
-    wheel3_s.color = wheel_color
-    space.add(wheel3_b, wheel3_s)
-    sprites.append((wheel3_b, wheel3_s))
+    # mass = 10
+    # radius = 25
+    # moment = pymunk.moment_for_circle(mass, 20, radius)
+    # wheel3_b = pymunk.Body(mass, moment)
+    # wheel3_s = pymunk.Circle(wheel3_b, radius)
+    # wheel3_s.friction = 1.5
+    # wheel3_s.color = wheel_color
+    # space.add(wheel3_b, wheel3_s)
+    # sprites.append((wheel3_b, wheel3_s))
 
     # chassis
     mass = 100
@@ -62,23 +159,23 @@ def create_rover(space, x, y):
 
     wheel1_b.position = pos + (-55, 0)
     wheel2_b.position = pos + (55, 0)
-    wheel3_b.position = pos + (0, 90)
+    # wheel3_b.position = pos + (0, 90)
     chassis_b.position = pos + (0, 30)
 
     space.add(
         pymunk.PinJoint(wheel1_b, chassis_b, (0, 0), (-25, -15)),
         pymunk.PinJoint(wheel1_b, chassis_b, (0, 0), (-25,  15)),
         pymunk.PinJoint(wheel2_b, chassis_b, (0, 0), (25,  -15)),
-        pymunk.PinJoint(wheel2_b, chassis_b, (0, 0), (25,   15)),
-        pymunk.PinJoint(wheel3_b, chassis_b, (0, 0), (-25, 15)),
-        pymunk.PinJoint(wheel3_b, chassis_b, (0, 0), (25, 15))
+        pymunk.PinJoint(wheel2_b, chassis_b, (0, 0), (25,   15))
+        # pymunk.PinJoint(wheel3_b, chassis_b, (0, 0), (-25, 15)),
+        # pymunk.PinJoint(wheel3_b, chassis_b, (0, 0), (25, 15))
     )
 
     speed = 0
     motors = [
         pymunk.SimpleMotor(wheel1_b, chassis_b, speed),
         pymunk.SimpleMotor(wheel2_b, chassis_b, speed),
-        pymunk.SimpleMotor(wheel3_b, chassis_b, speed)
+        # pymunk.SimpleMotor(wheel3_b, chassis_b, speed)
     ]
     space.add(*motors)
     return motors, chassis_b
@@ -109,6 +206,7 @@ while previous[0] < 9000:
 
     previous = (x, y)
 
+pog_car = create_pogo(space, 400, 600)
 
 motors, car = create_rover(space, 200, 600)
 acc = 8
@@ -165,26 +263,18 @@ while running:
         elif isinstance(shape, pymunk.Poly):
             vertices = shape.get_vertices()
             transformed_vertices = [v.rotated(body.angle) + body.position for v in vertices]
-            screen_vertices = [to_pygame(v, screen) + scroll for v in transformed_vertices]
+            try:
+                screen_vertices = [to_pygame(v, screen) + scroll for v in transformed_vertices]
+            except Exception as e:
+                print(body.angle)
+                print(vertices)
+                print(transformed_vertices)
+                raise e
             pygame.draw.polygon(screen, (255, 0, 0), screen_vertices)
         elif isinstance(shape, pymunk.Segment):
             vertices = shape.a, shape.b
             transformed_vertices = [v.rotated(body.angle) + body.position for v in vertices]
             screen_vertices = [to_pygame(v, screen) + scroll for v in transformed_vertices]
             pygame.draw.line(screen, (0, 255, 0), *screen_vertices, int(shape.radius))
-            
 
     pygame.display.flip()
-
-    frames.append(pygame.surfarray.array3d(screen))
-
-
-# write video
-fourcc = VideoWriter_fourcc(*'MPEG')
-video = VideoWriter("./demo.avi", fourcc, float(fps), (900, 600))
-
-for frame in frames:
-    frame.dtype = np.uint8
-    video.write(frame)
-
-video.release()
